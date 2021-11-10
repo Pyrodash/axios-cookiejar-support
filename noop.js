9aad93fe0bdd733ce358bc1ced810d2b81e8e320
Jackie <18378976+Pyrodash@users.noreply.github.com>
2021-11-10 23:13:51 +0200

    Added React Native support

--- noop.js
+++ noop.js
@@ -1,8 +1,12 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
 
-function noop(axios) {
+let noop = function(axios) {
   return axios;
+}
+
+if (typeof navigator !== 'undefined' && navigator.product === 'ReactNative') {
+  noop = require('./dist/index').wrapper
 }
 
 exports.wrapper = noop;
